df9041cb3b5a5cc07ace8135a3a3f337e4d16758
Wil <github@william-burns.com>
2017-05-19 15:37:29 -0400

    Show active teams on scoreboard

--- spec/features/admin_scoreboard_spec.rb
+++ spec/features/admin_scoreboard_spec.rb
@@ -1,16 +1,36 @@
 require 'rails_helper'
 
 RSpec.feature 'Admin scoreboard', type: :feature do
+  let(:contest) { Contest.create! }
+
   scenario 'admin scoreboard is accessible to admins' do
     # TODO Update when authorization is in place
-    visit admin_scoreboard_path(logged_in: true)
+    visit admin_contest_scoreboard_path(contest, logged_in: true)
 
-    expect(current_path).to eq(admin_scoreboard_path)
+    expect(current_path).to eq(admin_contest_scoreboard_path(contest))
   end
 
   scenario 'admin scoreboard is not accessible to participants' do
-    visit admin_scoreboard_path
+    visit admin_contest_scoreboard_path(contest)
 
     expect(current_path).to eq(root_path)
+  end
+
+  scenario 'a team without activity is not shown', :include_contest do
+    team = Team.create!(contest: contest, name: 'Team 1')
+
+    visit admin_contest_scoreboard_path(contest, logged_in: true)
+
+    expect(page).to_not have_text('Team 1')
+  end
+
+  scenario 'a team with activity is shown' do
+    team = Team.create!(contest: contest, name: 'Team 1')
+    problem = Problem.create!(contest: contest)
+    submission = Submission.create!(team: team, problem: problem)
+
+    visit admin_contest_scoreboard_path(contest, logged_in: true)
+
+    expect(page).to have_text('Team 1')
   end
 end
