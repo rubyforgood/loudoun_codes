a1630f7ec21f96540c669a16da1799b95a68bfcd
Brandon Rice <brandon@blrice.net>
2017-05-19 14:27:08 -0400

    Use a Procfile to get up and running.

--- Gemfile
+++ Gemfile
@@ -46,4 +46,5 @@ group :development do
   gem 'listen', '>= 3.0.5', '< 3.2'
   gem 'rubocop-rails'
   gem 'pry-byebug', '~> 3.4'
+	gem 'foreman'
 end
--- config/puma.rb
+++ config/puma.rb
@@ -9,7 +9,7 @@ threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port        ENV.fetch("PORT") { 3000 }
+port ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
@@ -54,3 +54,6 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+pidfile 'tmp/pids/puma.pid'
+state_path 'tmp/pids/puma.state'
