35faa9f0f35dacfd31de56c3b034e485ec487202
bennacer860 <bennacer860@gmail.com>
2017-05-19 11:26:57 -0400

    update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170519150604) do
+ActiveRecord::Schema.define(version: 20170519151012) do
 
   create_table "admins", force: :cascade do |t|
     t.string "username"
     t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "contests", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
