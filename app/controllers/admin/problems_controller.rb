20d1ae87ff34edd28e6f60c5a1dd6c5d1be1d44a
Chris Flipse <cflipse@gmail.com>
2017-05-19 17:14:20 -0400

    Fill in the create and show actions for problems

--- app/controllers/admin/problems_controller.rb
+++ app/controllers/admin/problems_controller.rb
@@ -6,5 +6,21 @@ module Admin
       @problem = @contest.problems.build
     end
 
+    def create
+      @contest = Contest.instance
+      @problem = @contest.problems.create problem_parameters
+    end
+
+    def show
+      @contest = Contest.instance
+      @problem = @contest.problems.find(params[:id])
+    end
+
+  private
+
+    def problem_parameters
+      params.require(:problem).permit(:name, :description)
+    end
+
   end
 end
