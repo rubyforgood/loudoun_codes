38157c80b7950ef4ea04963c39c5aee616a4d90c
Chris Flipse <cflipse@gmail.com>
2017-05-19 16:01:52 -0400

    Generate controller for admin/contests

---
+++ app/controllers/admin/contests_controller.rb
@@ -0,0 +1,4 @@
+module Admin
+  class ContestsController < ApplicationController
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,9 @@ Rails.application.routes.draw do
     get 'admin/contests/:contest_id/scoreboard' => 'scoreboard#show', as: :admin_contest_scoreboard
   end
 
+  namespace 'admin' do
+    get 'contest' => 'contests#show'
+  end
+
   root to: 'welcome#index'
 end
