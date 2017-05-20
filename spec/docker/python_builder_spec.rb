7599fadd3e09751e40388f22bfd468dfaa43ef0b
Daniel P. Clark <6ftdan@gmail.com>
2017-05-20 15:46:06 -0400

    remove use of temp directory for docker tests

--- spec/docker/python_builder_spec.rb
+++ spec/docker/python_builder_spec.rb
@@ -1,50 +1,41 @@
 require 'rails_helper'
-require_relative '../support/docker/python'
 
 RSpec.describe 'OmniBuilder with Python', type: :docker do
   describe 'It works with docker command and barebone python image' do
-    include Support::Docker::Python
+    let(:dir) { Dir.pwd + '/spec/fixtures/docker/' }
 
     it 'good entry submission`' do
-      Dir.mktmpdir { |dir|
-        Dir.chdir(dir) {
-          input = Docker::Input.create(File.join(dir, 'ProblemA.in'), in_file)
-          output = Docker::Output.create(File.join(dir, 'ProblemA.out'), out_file)
-          good_entry = Docker::Entry.create(File.join(dir, 'ProblemA.py'), good_entry_file)
+      input = Docker::Input.new(Pathname.new File.join(dir, 'ProblemA.in'))
+      output = Docker::Output.new(Pathname.new File.join(dir, 'ProblemA.out'))
+      good_entry = Docker::Entry.new(Pathname.new File.join(dir, 'good', 'ProblemA.py'))
 
-          expect(File.exist? input.path).to be_truthy
-          expect(File.exist? output.path).to be_truthy
-          expect(File.exist? good_entry.path).to be_truthy
+      expect(File.exist? input.path).to be_truthy
+      expect(File.exist? output.path).to be_truthy
+      expect(File.exist? good_entry.path).to be_truthy
 
-          builder = Docker::PythonBuilder.new(dir)
+      builder = Docker::PythonBuilder.new(dir)
 
-          sys_exec(builder.build(good_entry, input, output))
-          expect(err).to eq('')
-          expect(out).to eq('')
-          expect(@exitstatus).to eq(0)
-        }
-      }
+      sys_exec(builder.build(good_entry, input, output))
+      expect(err).to eq('')
+      expect(out).to eq('')
+      expect(@exitstatus).to eq(0)
     end
 
     it 'bad entry submission`' do
-      Dir.mktmpdir { |dir|
-        Dir.chdir(dir) {
-          input = Docker::Input.create(File.join(dir, 'ProblemA.in'), in_file)
-          output = Docker::Output.create(File.join(dir, 'ProblemA.out'), out_file)
-          bad_entry = Docker::Entry.create(File.join(dir, 'ProblemB.py'), bad_entry_file)
+      input = Docker::Input.new(Pathname.new File.join(dir, 'ProblemA.in'))
+      output = Docker::Output.new(Pathname.new File.join(dir, 'ProblemA.out'))
+      bad_entry = Docker::Entry.new(Pathname.new File.join(dir, 'bad', 'ProblemA.py'))
 
-          expect(File.exist? input.path).to be_truthy
-          expect(File.exist? output.path).to be_truthy
-          expect(File.exist? bad_entry.path).to be_truthy
+      expect(File.exist? input.path).to be_truthy
+      expect(File.exist? output.path).to be_truthy
+      expect(File.exist? bad_entry.path).to be_truthy
 
-          builder = Docker::PythonBuilder.new(dir)
+      builder = Docker::PythonBuilder.new(dir)
 
-          sys_exec(builder.build(bad_entry, input, output))
-          expect(err).to eq('')
-          expect(out).to_not eq('')
-          expect(@exitstatus).to_not eq(0)
-        }
-      }
+      sys_exec(builder.build(bad_entry, input, output))
+      expect(err).to eq('')
+      expect(out).to_not eq('')
+      expect(@exitstatus).to_not eq(0)
     end
   end
 end
--- spec/docker/ruby_builder_spec.rb
+++ spec/docker/ruby_builder_spec.rb
@@ -1,50 +1,41 @@
 require 'rails_helper'
-require_relative '../support/docker/ruby'
 
 RSpec.describe 'OmniBuilder with Ruby', type: :docker do
   describe 'It works with docker command and barebone ruby image' do
-    include Support::Docker::Ruby
+    let(:dir) { Dir.pwd + '/spec/fixtures/docker/' }
 
     it 'good entry submission`' do
-      Dir.mktmpdir { |dir|
-        Dir.chdir(dir) {
-          input = Docker::Input.create(File.join(dir, 'ProblemA.in'), in_file)
-          output = Docker::Output.create(File.join(dir, 'ProblemA.out'), out_file)
-          good_entry = Docker::Entry.create(File.join(dir, 'ProblemA.rb'), good_entry_file)
+      input = Docker::Input.new(Pathname.new File.join(dir, 'ProblemA.in'))
+      output = Docker::Output.new(Pathname.new File.join(dir, 'ProblemA.out'))
+      good_entry = Docker::Entry.new(Pathname.new File.join(dir, 'good', 'ProblemA.rb'))
 
-          expect(File.exist? input.path).to be_truthy
-          expect(File.exist? output.path).to be_truthy
-          expect(File.exist? good_entry.path).to be_truthy
+      expect(File.exist? input.path).to be_truthy
+      expect(File.exist? output.path).to be_truthy
+      expect(File.exist? good_entry.path).to be_truthy
 
-          builder = Docker::RubyBuilder.new(dir)
+      builder = Docker::RubyBuilder.new(dir)
 
-          sys_exec(builder.build(good_entry, input, output))
-          expect(err).to eq('')
-          expect(out).to eq('')
-          expect(@exitstatus).to eq(0)
-        }
-      }
+      sys_exec(builder.build(good_entry, input, output))
+      expect(err).to eq('')
+      expect(out).to eq('')
+      expect(@exitstatus).to eq(0)
     end
 
     it 'bad entry submission`' do
-      Dir.mktmpdir { |dir|
-        Dir.chdir(dir) {
-          input = Docker::Input.create(File.join(dir, 'ProblemA.in'), in_file)
-          output = Docker::Output.create(File.join(dir, 'ProblemA.out'), out_file)
-          bad_entry = Docker::Entry.create(File.join(dir, 'ProblemB.rb'), bad_entry_file)
+      input = Docker::Input.new(Pathname.new File.join(dir, 'ProblemA.in'))
+      output = Docker::Output.new(Pathname.new File.join(dir, 'ProblemA.out'))
+      bad_entry = Docker::Entry.new(Pathname.new File.join(dir, 'bad', 'ProblemA.rb'))
 
-          expect(File.exist? input.path).to be_truthy
-          expect(File.exist? output.path).to be_truthy
-          expect(File.exist? bad_entry.path).to be_truthy
+      expect(File.exist? input.path).to be_truthy
+      expect(File.exist? output.path).to be_truthy
+      expect(File.exist? bad_entry.path).to be_truthy
 
-          builder = Docker::RubyBuilder.new(dir)
+      builder = Docker::RubyBuilder.new(dir)
 
-          sys_exec(builder.build(bad_entry, input, output))
-          expect(err).to eq('')
-          expect(out).to_not eq('')
-          expect(@exitstatus).to_not eq(0)
-        }
-      }
+      sys_exec(builder.build(bad_entry, input, output))
+      expect(err).to eq('')
+      expect(out).to_not eq('')
+      expect(@exitstatus).to_not eq(0)
     end
   end
 end
