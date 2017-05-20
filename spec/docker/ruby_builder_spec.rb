b6b4fdfa1e5127a7ed110999dd1afb9eef1da658
Daniel P. Clark <6ftdan@gmail.com>
2017-05-20 12:06:55 -0400

    split happy and bad path tests out

--- spec/docker/ruby_builder_spec.rb
+++ spec/docker/ruby_builder_spec.rb
@@ -4,18 +4,15 @@ include Support::Docker::Ruby
 
 RSpec.describe 'OmniBuilder with Ruby', type: :docker do
   describe 'It works with docker command and barebone ruby image' do
-    it 'the big picture result here' do
+
+    it 'good entry submission`' do
       Dir.mktmpdir { |dir|
         Dir.chdir(dir) {
           input = Docker::Input.create(File.join(dir, 'ProblemA.in'), in_file)
           output = Docker::Output.create(File.join(dir, 'ProblemA.out'), out_file)
           good_entry = Docker::Entry.create(File.join(dir, 'ProblemA.rb'), good_entry_file)
-          bad_entry = Docker::Entry.create(File.join(dir, 'ProblemB.rb'), bad_entry_file)
 
-          expect(File.exist? input.path).to be_truthy
-          expect(File.exist? output.path).to be_truthy
           expect(File.exist? good_entry.path).to be_truthy
-          expect(File.exist? bad_entry.path).to be_truthy
 
           builder = Docker::RubyBuilder.new(dir)
 
@@ -23,6 +20,20 @@ RSpec.describe 'OmniBuilder with Ruby', type: :docker do
           expect(out).to eq('')
           expect(err).to eq('')
           expect(@exitstatus).to eq(0)
+        }
+      }
+    end
+
+    it 'bad entry submission`' do
+      Dir.mktmpdir { |dir|
+        Dir.chdir(dir) {
+          input = Docker::Input.create(File.join(dir, 'ProblemA.in'), in_file)
+          output = Docker::Output.create(File.join(dir, 'ProblemA.out'), out_file)
+          bad_entry = Docker::Entry.create(File.join(dir, 'ProblemB.rb'), bad_entry_file)
+
+          expect(File.exist? bad_entry.path).to be_truthy
+
+          builder = Docker::RubyBuilder.new(dir)
 
           sys_exec(builder.build(bad_entry, input, output))
           expect(out).to_not eq('')
