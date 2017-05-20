4d84db6c1aedbf2a34c8b7bba8f8501b6fca03f6
bennacer860 <bennacer860@gmail.com>
2017-05-20 10:20:55 -0400

    in tests use create! instead of create

--- spec/features/admin_authentication_spec.rb
+++ spec/features/admin_authentication_spec.rb
@@ -2,21 +2,21 @@ require 'rails_helper'
 
 RSpec.feature 'Admin Authentication', type: :feature do
   scenario 'admin login successfully' do
-    Administrator.create(username: "someone@example.tld", password: "somepassword")
+    Administrator.create!(username: "someone@example.tld", password: "somepassword")
     visit root_path
     login("someone@example.tld", "somepassword")
     expect(page).to have_content "You have successfully logged in"
   end
 
   scenario 'admin login unsuccessfully' do
-    Administrator.create(username: "someone@example.tld", password: "somepassword")
+    Administrator.create!(username: "someone@example.tld", password: "somepassword")
     visit root_path
     login("someone@example.tld", "someotherpassword")
     expect(page).to have_content "You have entered the wrong credentials"
   end
 
   scenario 'admin logout successfully' do
-    Administrator.create(username: "someone@example.tld", password: "somepassword")
+    Administrator.create!(username: "someone@example.tld", password: "somepassword")
     visit root_path
     login("someone@example.tld", "somepassword")
     click_link "Logout"
