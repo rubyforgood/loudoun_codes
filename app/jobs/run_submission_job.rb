0da08d63e953525f4e254066fb18b90e648737aa
Chris Hoffman <choffman@optoro.com>
2017-05-20 23:04:55 -0400

    comment out rough draft of runner integration.

--- app/jobs/run_submission_job.rb
+++ app/jobs/run_submission_job.rb
@@ -5,26 +5,26 @@ class RunSubmissionJob < ApplicationJob
 
   def perform(submission_id)
     submission = Submission.find_by_id!(submission_id)
-    runner     = SubmissionRunners.runner_for_submission(submission)
 
     submission_result = SubmissionResult.create!({
       submission_id: submission.id,
     })
 
-    runner.call
+    # runner = SubmissionRunners.runner_for_submission(submission)
+    # runner.call
 
-    if runner.run_succeeded?
-      submission_result.passed = true
-    else
-      runner.errors.each do |runner_phase, error|
-        SubmissionResultNotification.create!({
-          submission_result_id: submission_result.id,
-          runner_phase:         runner_phase.to_s,
-          message:              error.message,
-        })
-      end
-    end
+    # if runner.run_succeeded?
+    #   submission_result.passed = true
+    # else
+    #   runner.errors.each do |runner_phase, error|
+    #     SubmissionResultNotification.create!({
+    #       submission_result_id: submission_result.id,
+    #       runner_phase:         runner_phase.to_s,
+    #       message:              error.message,
+    #     })
+    #   end
+    # end
 
-    submission_result.save!
+    # submission_result.save!
   end
 end
