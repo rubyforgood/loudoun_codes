af61cd4b317d79f1f047421056b995582098cc38
Chris Hoffman <choffman@optoro.com>
2017-05-21 11:41:09 -0400

    ActiveJob integrated with SubmissionRunners

--- spec/jobs/run_submission_job_spec.rb
+++ spec/jobs/run_submission_job_spec.rb
@@ -1,12 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe RunSubmissionJob, type: :job do
-  it 'creates a submission result' do
-    contest    = Contest.create
-    team       = Team.create(contest: contest)
-    problem    = Problem.create(contest: contest)
-    submission = Submission.create(problem: problem, team: team)
+  let(:contest) { Contest.create! }
+  let(:team)    { contest.teams.create! }
+  let(:problem) do
+    Problem.create_from_files!({
+      contest:     contest,
+      name:        "CompilesAndRuns",
+      output_file: fixtures.join("Output"),
+      input_file:  fixtures.join("Input"),
+    })
+  end
+  let(:submission) do
+    Submission.create_from_file({
+      problem:  problem,
+      team:     team,
+      filename: fixtures.join("#{problem.name}.java"),
+    }).tap(&:validate!)
+  end
+  let(:fixtures) { Pathname.new(Rails.root).join("spec/fixtures/submission_runners/java") }
 
+  it 'creates a submission result' do
     RunSubmissionJob.new.perform(submission.id)
 
     expect(submission.reload.submission_results.count).to eq(1)
