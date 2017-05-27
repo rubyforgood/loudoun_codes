78132f081a2a37af09b6ee8c00d0b2e2cddc50d9
Chris Hoffman <choffman@optoro.com>
2017-05-27 16:46:43 -0400

    we can use SourceFile#without_extension here instead

--- lib/submission_runners/cpp.rb
+++ lib/submission_runners/cpp.rb
@@ -1,23 +1,18 @@
 # -*- coding: utf-8 -*-
 require 'submission_runners/base'
-require_relative '../../lib/docker/helpers'
 
 module SubmissionRunners
   class Cpp < Base
-    attr_reader :entry
-    include Docker::Helpers
-
     def self.image
       "gcc:7.1"
     end
 
     def build
-      @entry = EntryFile(source_file)
-      docker_run("g++", "-o", entry.basename, entry.filename, chdir: submission_dir)
+      docker_run("g++", "-o", source_file.without_extension, source_file, chdir: submission_dir)
     end
 
     def run
-      docker_run("./#{entry.basename}")
+      docker_run("./#{source_file.without_extension}")
     end
   end
 end
